4f3858060423bda5ea2c730dc04ab136141dd04d
vinicius.freitas0 <vinicius.freitas@anchieta.br>
2022-04-13 16:47:26 -0300

    :hammer: create app structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div >
-      <h1>Hello World</h1>
-    </div>
+    <>
+    <Head>
+      <title>Início | ig.news</title>
+    </Head>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </>
   )
 }
