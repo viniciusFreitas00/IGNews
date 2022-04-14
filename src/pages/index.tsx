f3a8623a0450b755df3a4ab85b95a35394d2f918
vinicius.freitas0 <vinicius.freitas@anchieta.br>
2022-04-14 17:16:31 -0300

    :sparkles: connecting app with stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,60 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
+import { stripe } from '../services/stripe';
+
 import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
     <Head>
       <title>Home | ig.news</title>
     </Head>
      <main className={styles.contentContainer}>
-       <section className={styles.hero}>
+        <section className={styles.hero}>
           <span>👏 Hey, welcome</span>
           
           <h1>News about the <span>React</span> world.</h1>
 
           <p>
             Get access to all the publications <br />
-            <span>For $9.90 month</span>
+            <span>For {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
-       </section>
+          <SubscribeButton priceId={product.priceId}/>
+        </section>
 
-       <img src="/images/avatar.svg" alt="Girl coding" />
+        <Image src="/images/avatar.svg" alt="Girl coding" width="336" height="521"/>
      </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KoUTkGnzvhXg1XMx1s7Ap0K')
+
+  const product =  {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product
+    }
+  }
 }
