9aa539e6cc9c63d23716bbc33832fe5eb101bc90
vinicius.freitas0 <vinicius.freitas@anchieta.br>
2022-04-20 18:16:22 -0300

    :lipstick: creating post component

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head';
+import styles from './style.module.scss';
+
+export default function Posts() {
+  return (
+    <> 
+      <Head>
+        <title>Post | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+          </a>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+          </a>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.</p>
+          </a>
+        </div>
+      </main>
+    </>
+  )
+}
